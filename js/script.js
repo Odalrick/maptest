313493d48613275919bb3ba143c8719c8fc16902
Ulrik Långström <odalrick@gmail.com>
2012-03-07 20:49:58 +0100

    Added the map-thingie I made the other day.

--- js/script.js
+++ js/script.js
@@ -6,3 +6,41 @@
 
 
 
+document.addEvent( 'domready', function () {
+    var map_view = $( 'map_view' );
+    log( map_view );
+    if ( map_view ) {
+        var map = {
+            nodes: [
+                ['sea', 'sea', 'sea', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass' ],
+                ['sea', 'sea', 'sea', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass' ],
+                ['sea', 'sea', 'sea', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass' ],
+                ['sea', 'sea', 'sea', 'sea', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass' ],
+                ['sea', 'grass', 'grass', 'sea', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass' ],
+                ['grass', 'grass', 'grass', 'sea', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass' ],
+                ['grass', 'grass', 'grass', 'sea', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass' ],
+                ['grass', 'grass', 'grass', 'sea', 'sea', 'sea', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass' ],
+                ['grass', 'grass', 'grass', 'grass', 'grass', 'sea', 'sea', 'sea', 'sea', 'sea', 'sea', 'sea', 'sea', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass' ],
+                ['grass', 'grass', 'grass', 'grass', 'grass', 'sea', 'grass', 'grass', 'grass', 'grass', 'grass', 'sea', 'sea', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass' ],
+                ['grass', 'grass', 'grass', 'grass', 'grass', 'sea', 'grass', 'grass', 'grass', 'grass', 'grass', 'sea', 'sea', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass' ],
+                ['grass', 'grass', 'grass', 'grass', 'grass', 'sea', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass' ],
+                ['grass', 'grass', 'grass', 'grass', 'grass', 'sea', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass' ],
+                ['grass', 'grass', 'grass', 'grass', 'grass', 'sea', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass' ],
+                ['grass', 'grass', 'grass', 'grass', 'grass', 'sea', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass' ],
+                ['grass', 'grass', 'grass', 'grass', 'grass', 'sea', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass' ],
+                ['grass', 'grass', 'grass', 'grass', 'grass', 'sea', 'sea', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass' ],
+                ['grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'sea', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass castle', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass' ],
+                ['grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'sea', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass' ],
+                ['grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'sea', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass', 'grass' ]
+            ]
+        }
+        map.nodes.each( function ( row, x ) {
+            var row_element = new Element( 'div.row' );
+            map_view.adopt( row_element );
+            row.each( function ( tile, y ) {
+                new Element( 'div', {'class': tile + ' tile' } ).inject( row_element );
+            } );
+        } );
+    }
+} );
+
